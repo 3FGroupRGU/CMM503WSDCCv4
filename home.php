4d0120f75b95e2bda2d73e9fa18287efe3e07493
HenryRL <1300427@rgu.ac.uk>
2016-03-07 14:29:18 +0000

    Update 1.0B Style Test Index2

---
+++ home.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+<h1>Hello my lovely. What you doing here then?</h1>
+
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,7 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: henry
- * Date: 07/03/2016
- * Time: 11:33
- */
+include("connect.php"); //Establishing connection to our database
+if (empty($_POST ["username"]) || empty($_POST ["password"])) {
+    echo "Both fields are required.";
+} else {
+    $username = $_POST ["username"];
+    $password = $_POST ["password"];
+    echo $username;
+    echo $password;
+    $sql = "SELECT * FROM users WHERE username='$username' and password='$password'";
+    $result = mysqli_query($db, $sql);
+    if (mysqli_num_rows($result) == 1) {
+        header("location: home.php"); // Redirecting to another page
+    } else {
+        echo "Incorrect username or password";
+    }
+}
+?>
