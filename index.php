82f7cacee43759de549ab6f61cbabedd6ebaabb2
HenryRL <1300427@rgu.ac.uk>
2016-03-09 13:46:58 +0000

    Update 1.0B Style Test Index63

--- index.php
+++ index.php
@@ -38,9 +38,9 @@
             <br><br>
             <form method="post" action="login.php">
                 <label>Username:</label><br>
-                <input type="text" name="username" placeholder="username" /><br><br>
+                <input type="text" name="username" placeholder="username" required="required"/><br><br>
                 <label>Password:</label><br>
-                <input type="password" name="password" placeholder="password" />  <br>
+                <input type="password" name="password" placeholder="password" required="required"/>  <br>
                 <label>E-mail</label><br>
                 <input type="email" name="email" placeholder="e-mail" />
                 <br>
