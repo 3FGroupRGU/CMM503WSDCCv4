d29c71b9985cf6e07ed67f286e12ce69885c808a
HenryRL <1300427@rgu.ac.uk>
2016-03-22 09:52:08 +0000

    Update 1.0B Style Test Index161

--- bug.php
+++ bug.php
@@ -1,35 +1,59 @@
+<?php
+include("connect.php");
+$sql="SELECT * FROM bugs WHERE bugs.ID=".$_GET["id"];
+$result=mysqli_query($db,$sql);
+$row=mysqli_fetch_assoc($result);
+$bugTitle=$row['title'];
+$bugID=$row['ID'];
+$bugDesc=$row['desc'];
+
+echo"<h2>".$bugTitle."</h2>";
+echo"<p>".$bugDesc."</p>";
+
+$sql="SELECT * FROM comments WHERE bugID=".$_GET["id"];
+$result=mysqli_query($db,$sql);
+while ($row=mysqli_fetch_assoc($result)){
+    $commentTitle=$row['title'];
+    $commetn=$row['comment'];
+
+    echo'<h3>'.$title.'</h3>';
+    echo'<p>'.$comment.'</p>';
+}
+?>
 <!doctype html>
+<head>
+    <meta charset="utf-8">
+    <title>Ginger Bugginess</title>
+    <link rel="stylesheet" href="layout.css" type="text/css" />
+    <link rel="stylesheet" href="menu.css" type="text/css" />
+</head>
 <body>
 <div id="content">
-    <?php
-    include("connect.php");
-    $sql="SELECT * FROM bugs WHERE bugs.ID=".$_GET["id"];
-    $result=mysqli_query($db,$sql);
-    $row=mysqli_fetch_assoc($result);
-    $bugTitle=$row['title'];
-    $bugID=$row['ID'];
-    $bugDesc=$row['desc'];
+    <form class="bugInfo">
+   <label>Bug Title:</label>
+    <input type="text" name="BugTitle" placeholder="Bug Title"><br>
+    <label>Bug Description</label>
+    <input type="text" name="BugDesc" placeholder="Bug Description"><br>
+        <label>User ID</label>
+        <input type="text" name="$userID" placeholder="User ID"><br>
+        <label>Date Bug Raised</label>
+        <input type="date" name="date" placeholder="Date Of Bug"><br>
+        <label>Date Bug Fixed</label>
+        <input type="date" name="date" placeholder="Date Fixed"><br>
+        <?php
+            if (isset($_POST['SUBMIT1'])) {
 
-    echo"<h2>".$bugTitle."</h2>";
-    echo"<p>".$bugDesc."</p>";
-
-    $sql="SELECT * FROM comments WHERE bugID=".$_GET["id"];
-    $result=mysqli_query($db,$sql);
-    while ($row=mysqli_fetch_assoc($result)){
-        $commentTitle=$row['title'];
-        $commetn=$row['comment'];
-
-        echo'<h3>'.$title.'</h3>';
-        echo'<p>'.$comment.'</p>';
-    }
-    ?>
+            }
+        if (isset($ch1=='Fixed')) {
+            $ch1='checked';
+        }
+        ?>
+        <input type="Checkbox" name="ch1" value="Fixed">
+        <?php
+            print $ch1;
+        ?>
+    </form>
 </div>
 </body>
 
 </html>
-/**
- * Created by PhpStorm.
- * User: 1300427
- * Date: 16/03/2016
- * Time: 15:37
- */
--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-
+<?php 
+session_start();
+    
+if ((isset($_SESSION['username']) != ''))
+{
+header('Location: home.php');
+}
+session_destroy()
+?>
 <!doctype html>
 <!--design has been followed from Simpletut.com via https://www.youtube.com/watch?v=Qqcj4nYkcks'-->
 <html>
@@ -11,36 +19,36 @@
 
 <body>
 <div id="holder">
-<div id="header">
-    <h1>Ginger Bugginess Fault Tracker</h1>
-</div>
-<div id="NavBar">
-    <nav>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Login</a></li>
-            <li><a href="#">Register</a></li>
-            <li><a href="#">Forgot Login</a></li>
-        </ul>
-    </nav>
-</div>
-<div id="content">
-    <div id="pageheading">
-        <h1>Welcome</h1>
+    <div id="header">
+        <h1>Ginger Bugginess Fault Tracker</h1>
     </div>
-    <div id="contentleft">
-        <h2>Welcome to the Ginger Bugginess fault logging page.</h2><br>
-        <h3>Site Down For Maintenance</h3><br>
-        <p>This site is temporarily unavailable.</p><br>
-        <p>This is due to the numpty who's working on it being a pillock and having to re-wrie all his PHP.</p>
+    <div id="NavBar">
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Register</a></li>
+                <li><a href="ForgotPassword.php">Forgot Login</a></li>
+            </ul>
+        </nav>
     </div>
-    <div id="contentright">
-        <h2>Nothing Here to See!</h2><br>
-        <p>Please be good little sheep and move along.....</p>
+    <div id="content">
+        <div id="pageheading">
+            <h1>Welcome</h1>
+        </div>
+        <div id="contentleft">
+            <h2>Welcome to the Ginger Bugginess fault logging page.</h2><br>
+            <h3>Before you begin.</h3><br>
+            <p>This site is for registered and authorised users only. On this site you can upload your fault logs or enter a new fault manually. The status of which will be assessed and updated by the IT team as they work through their call logs.</p><br>
+            <p>Please login or register to continue on this site.</p>
+        </div>
+        <div id="contentright">
             <div class="error"><?php //echo $error;?><?php //echo $username; echo $password;?></div>
+        </div>
     </div>
-</div>
-<div id="footer"></div>
+    <div id="footer"></div>
 </body>
 </html>
+
+
 
